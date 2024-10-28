982ae26d4d54f60d0597d520d5c3a5e23601e1a0
Sungchan Ahn <146975762+sungchanahn@users.noreply.github.com>
2024-10-29 01:27:41 +0900

    feat Moving Platform
    상호작용 시 출발

--- Assets/Scripts/Object/UtilObject/MovingPlatform.cs
+++ Assets/Scripts/Object/UtilObject/MovingPlatform.cs
@@ -1,11 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class MovingPlatform : UtilObject
 {
+    public Transform start;
+    public Transform arrival;
+
+    public float speed;
+    public float waitSecond;
+
     public override void OnInteract()
     {
-        // 출발 시키기
+        StartCoroutine(MovePlatform());
+    }
+
+    IEnumerator MovePlatform()
+    {
+        while (Vector3.Distance(transform.position, arrival.position) > 0.1f)
+        {
+            yield return null;
+            MoveStartToArrival();
+        }
+        yield return new WaitForSeconds(waitSecond);
+
+        while (Vector3.Distance(transform.position, start.position) > 0.1f)
+        {
+            yield return null;
+            MoveArrivalToStart();
+        }
+    }
+
+    private Vector3 SetDirection(Vector3 startPosition, Vector3 arrivalPosition)
+    {
+        return (arrivalPosition - startPosition).normalized;
+    }
+
+    private void MoveStartToArrival()
+    {
+        Vector3 direction = SetDirection(start.position, arrival.position);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void MoveArrivalToStart()
+    {
+        Vector3 direction = SetDirection(arrival.position, start.position);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.gameObject.transform.SetParent(null);
     }
 }
